cb7c134672104cf7ecfe0ca7e5cf7e5d87d3251d
rsantos88 <rasantos@it.uc3m.es>
2017-11-21 12:55:31 +0100

    new python example with Teo showing differents positions grasping a bottle

---
+++ example/python/grasping-bottle-example.py
@@ -0,0 +1,66 @@
+##
+# @ingroup teo_grasp_examples_py
+# \defgroup grasping-bottle-example grasping-bottle-example.py
+#
+# This is a first example of grasping with Teo using rightArm + trunk.
+#
+# <b>Legal</b> 
+#
+# Copyright: (C) 2017 Universidad Carlos III de Madrid
+#
+# Author: Raul de Santos Rico
+#
+# CopyPolicy: Released under the terms of the LGPLv2.1 or later, see license/LGPL.TXT
+
+from openravepy import *
+from numpy import *
+import numpy, time
+env=Environment()
+env.SetViewer('qtcoin')
+env.Load('/models/lab.env.xml')
+robot = env.GetRobots()[0]
+robot.SetActiveManipulator('rightArm_trunk')
+target = env.GetKinBody('bottle')
+gmodel = databases.grasping.GraspingModel(robot,target)
+
+if not gmodel.load():
+    gmodel.autogenerate()
+    # print 'generating grasping model (one time computation)'
+    # gmodel.init(friction=5,avoidlinks=[])
+    # gmodel.generate(approachrays=gmodel.computeBoxApproachRays(delta=0.02))
+    # gmodel.save()
+else: print("gmodel loaded...")
+
+validgrasps, validindicees = gmodel.computeValidGrasps(returnnum=50)
+print("validgrasps is ",len(validgrasps), "validindicees is ", len(validindicees))
+basemanip = interfaces.BaseManipulation(robot)
+with env:
+    initialvalues = robot.GetDOFValues(gmodel.manip.GetArmIndices())
+
+cont = 0
+# while 1:
+for validgrasp in validgrasps: #random.permutation(validgrasps):
+    print 'trying to grasp de object (attempt: %d)' %cont
+    try:
+        gmodel.moveToPreshape(validgrasp)
+    	print "move robot arm to grasp"
+    	Tgrasp = gmodel.getGlobalGraspTransform(validgrasp,collisionfree=False)	
+    	basemanip.MoveToHandPosition(matrices=[Tgrasp])
+    	robot.WaitForController(10)
+	taskmanip = interfaces.TaskManipulation(robot)
+	taskmanip.CloseFingers()
+	robot.WaitForController(10)
+	# time.sleep(5)   # delays for 5 seconds
+	with env:
+	    robot.Grab(target)
+        raw_input('press any key to release')
+        taskmanip.ReleaseFingers(target=target)
+        robot.WaitForController(10)
+        print 'initial values'
+        basemanip.MoveManipulator(initialvalues)
+        robot.WaitForController(10)
+
+	# break
+    except planning_error,e:
+ 	print 'try again: ',e
+    cont += 1
