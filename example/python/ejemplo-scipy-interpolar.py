3920c164f491f1a3163e345bbec83ce3b9c3df0b
rsantos88 <rasantos@it.uc3m.es>
2018-02-27 11:28:22 +0100

    finished interpolation example

--- example/python/ejemplo-scipy-interpolar.py
+++ example/python/ejemplo-scipy-interpolar.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import division
 
 #----------------------------------------------------------------------
 # Nombre:       ejemplo_scipy_interpolar.py
@@ -16,39 +14,39 @@ from __future__ import division
 #----------------------------------------------------------------------
 
 """
-Ejemplo de como hacer una interpolacion en python.
+Ejemplo de como hacer una interpolación en python de 10 ptos 
 """
 import scipy as sp                  # Importamos scipy como el alias sp
 from scipy import interpolate       # Importamos interpolate de scipy
 import matplotlib.pyplot as plt     # Importamos matplotlib.pyplot como el alias plt
 
 # Creamos el array dimensional
-x = sp.linspace(0,3,10) # Crea un array con valor inicial start, valor final stop y num elementos
+x = sp.linspace(1,10,10) # Crea un array con valor inicial start, valor final stop y num elementos
 
 # Evaluamos x en la funcion y = e^(-x/3) (generando nuestros "datos experimentales")
-y = 2*x**2+1 # ecuacion de la curva sp.exp(-x/3.0)
+y = 2*x**2+1 # ecuacion de la curva. Ej: sp.exp(-x/3.0)
 
 # Interpolamos
 interpolacion = interpolate.interp1d(x, y)
 
 # Creamos un nuevo array dimensional con mas puntos en el mismo intervalo
-x2 = sp.linspace(0,3,10)
+x2 = sp.linspace(1,10,10)
 
 # Evaluamos x2 en la interpolacion
 y2 = interpolacion(x2)
 
-for i in range (len(x2)-1):
+for i in range (len(x2)):
     print '(x, y) = ('+str(x2[i])+', '+str(y2[i])+')'
 
 
 # Creamos la figura
-# plt.figure
+plt.figure
 
 # Dibujamos las dos graficas juntas
 plt.plot(x, y, 'o', x2, y2, '-')
 
 # Añadimos la leyenda
-# plt.legend(('Datos conocidos', 'Datos experimentales'))
+plt.legend(('Puntos obtenidos', 'Interpolacion'))
 
 # añadimos las etiquetas
 plt.xlabel('Eje x')
