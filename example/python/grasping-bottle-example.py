940886eb6f394381c28462d3e1c6af6c4803365b
rsantos88 <rasantos@it.uc3m.es>
2018-03-19 18:04:56 +0100

    autogenerating ik with the purpouse of avoiding attening repositories with .so files

--- example/python/grasping-bottle-example.py
+++ example/python/grasping-bottle-example.py
@@ -15,20 +15,30 @@
 from openravepy import *
 from numpy import *
 import numpy, time
+from openravepy.misc import InitOpenRAVELogging
+InitOpenRAVELogging() # initialized with a logger. Allow us to log IKFast information 
 
 env = Environment()
 env.SetViewer('qtcoin')
 env.Load('/usr/local/share/teo-grasp/contexts/models/lab.env.xml')
 robot = env.GetRobots()[0]
-robot.SetActiveManipulator('leftArm_trunk')
+robot.SetActiveManipulator('rightArm_trunk')
+
+ikmodel = databases.inversekinematics.InverseKinematicsModel(robot, iktype=IkParameterizationType.Transform6D)
+if not ikmodel.load():
+    print 'Generating IK equation of: %s (one time computation)...' %robot.GetActiveManipulator().GetName()
+    ikmodel.autogenerate()
+else:
+    print 'Inverse Kinematic of %s loaded!' %robot.GetActiveManipulator().GetName()
+
 target = env.GetKinBody('bottle')
 gmodel = databases.grasping.GraspingModel(robot, target)
 
 if not gmodel.load():
     # gmodel.autogenerate()
-    print 'generating grasping model (one time computation)'
+    print 'Generating grasping model (one time computation)'
     gmodel.init(friction=0.8, avoidlinks=[])
-    gmodel.generate(approachrays=gmodel.computeBoxApproachRays(delta=0.08, normalanglerange=0,
+    gmodel.generate(approachrays=gmodel.computeBoxApproachRays(delta=0.08, normalanglerange=0, # delta=0.08 
                                                                directiondelta=0.40000000000000002))  # cuanto mas aumenta delta, menos rayos se generan para el grasp
     gmodel.save()
 else:
